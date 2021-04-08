4979c2099ad4db0490f86ac47c86ec56df8a0ec4
ranggaumar22 <82150603+ranggaumar22@users.noreply.github.com>
2021-04-08 04:48:48 -0700

    Add files via upload

---
+++ Pendaftaran dan Tabel Nilai.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title> Formulir Pendaftaran Online </title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<tr> 
+			<td> By Rangga Umar Pratama/11119005 </td>
+		</tr>
+		<div class="wrap">
+			<div class="container">
+				<h1> Formulir Pendaftaran </h1>
+				<form action="" method="">
+					<table>
+						<tr>
+							<td> Nama Depan </td>
+							<td> : </td>
+							<td><input type="text" name="Nama_Depan"></td>
+						</tr>
+						<tr>
+							<td> Nama Belakang </td>
+							<td> : </td>
+							<td><input type="text" name="Nama_Belakang"></td>
+						</tr>
+						<tr>
+							<td> Alamat </td>
+							<td> : </td>
+							<td><textarea name="alamat"></textarea></td>
+						</tr>
+						<tr>
+							<td> Tempat Lahir </td>
+							<td> : </td>
+							<td><input type="text" name="Tempat_Lahir"></td>
+						</tr>
+						<tr>
+							<td> Tanggal Lahir </td>
+							<td> : </td>
+							<td><input type="date" name="Tanggal_Lahir"></td>
+						</tr>
+						<tr>
+							<td> Jenis Kelamin </td>
+							<td> : </td>
+							<td>
+							<input type="radio" name="jk" value="Laki-laki"> Laki - laki
+							<input type="radio" name="jk" value="Perempuan"> Perempuan
+							</td>
+						</tr>
+						<tr>
+							<td> Nomor Telepon </td>
+							<td> : </td>
+							<td><input type="number" name="Nomor_Telepon"></td>
+						</tr>
+						<tr>
+							<td> Email </td>
+							<td> : </td>
+							<td><input type="text" name="Email"></td>
+						</tr>
+						<tr>
+							<td> Username </td>
+							<td> : </td>
+							<td><input type="text" name="Username"></td>
+						</tr>
+						<tr>
+							<td> Password </td>
+							<td> : </td>
+							<td><input type="Password" name="Password"></td>
+						</tr>
+						<tr>
+							<td> Konfirmasi Password </td>
+							<td> : </td>
+							<td><input type="Password" name="Konfirmasi_Password"></td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>	 	 
+						<tr>
+							<td> Foto Profil </td>
+							<td> : </td>
+							<td><input type="file" name="gambar"> <input type="submit" value="Upload"></td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+
+						<tr>
+							<td> </td>
+							<td> </td>
+						<td>
+							<button type="submit" name="Kirim">Kirim </button>
+							<button type="reset" name="Batal">Batal </button></td>
+						</tr>
+						<tr>
+							<td> </td>
+						</tr>
+					</table>
+				</form>
+			</div>
+		</div>
+		<br>
+		<h2>Tabel IP Per Semester</h2>
+          <table style="width:600px"border="1">
+              <tr style="background-color:#cfcfcf">
+              <th>SEMESTER</th>
+              <th>IPS</th>
+                </tr>
+              <tr>
+                <td style="text-align:center;">SEMESTER 1</td><td style="text-align:right">3.1</td>
+              </tr>
+              <tr>
+                <td style="text-align:center;">SEMESTER 2</td><td style="text-align:right">3.2</td>
+              </tr>
+              <tr>
+                <td style="text-align:center;">SEMESTER 3</td><td style="text-align:right">3.3</td>
+              </tr>
+              <tr> 
+                <td style="text-align:center;">SEMESTER 4</td><td style="text-align:right">3.4</td>
+              </tr>
+              <tr style="text-align:right;background-color=#FFCBCB;font-weight:bold">
+                <td style="text-align:center;">NILAI IPK &gt;&gt;&gt;</td>
+                <td style="text-align:center;">3.3</td>
+              </tr>
+          </br>
+          </table>
+	</body>
+</html>
